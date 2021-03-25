c702de57d67480756c5e9958c0fff2fd9c7ca262
y-miyazaki <ymiyazakixyz@gmail.com>
2021-03-25 13:16:03 +0000

    fixed.

---
+++ modules/aws/recipes/trusted_advisor/main.tf
@@ -0,0 +1,20 @@
+#--------------------------------------------------------------
+# Provides an EventBridge Rule resource.
+#--------------------------------------------------------------
+resource "aws_cloudwatch_event_rule" "this" {
+  name                = lookup(var.aws_cloudwatch_event_rule, "name", "trusted-advisor-cloudwatch-event-rule")
+  schedule_expression = lookup(var.aws_cloudwatch_event_rule, "schedule_expression", "cron(*/5 * * * ? *)")
+  description         = lookup(var.aws_cloudwatch_event_rule, "description", "Trusted Advisor event rule.")
+  is_enabled          = lookup(var.aws_cloudwatch_event_rule, "is_enabled", true)
+  tags                = var.tags
+}
+#--------------------------------------------------------------
+# Provides an EventBridge Target resource.
+#--------------------------------------------------------------
+resource "aws_cloudwatch_event_target" "this" {
+  rule = aws_cloudwatch_event_rule.this.name
+  arn  = lookup(var.aws_cloudwatch_event_target, "arn", null)
+  depends_on = [
+    aws_cloudwatch_event_rule.this
+  ]
+}
--- modules/aws/recipes/trusted_advisor/variables.tf
+++ modules/aws/recipes/trusted_advisor/variables.tf
@@ -6,20 +6,21 @@ variable "aws_cloudwatch_event_rule" {
     {
       # (Required) The name of the rule. If omitted, Terraform will assign a random, unique name. Conflicts with name_prefix.
       name = string
+      # (Optional) The scheduling expression. For example, cron(0 20 * * ? *) or rate(5 minutes). At least one of schedule_expression or event_pattern is required. Can only be used on the default event bus.
+      schedule_expression = string
       # (Optional) The description of the rule.
       description = string
-      # (Optional) The Amazon Resource Name (ARN) associated with the role that is used for target invocation.
-      role_arn = string
       # (Optional) Whether the rule should be enabled (defaults to true).
       is_enabled = bool
     }
   )
   description = "(Required) Provides an EventBridge Rule resource."
   default = {
-    name        = "trusted-advisor-cloudwatch-event-rule"
-    description = "Trusted Advisor event rule."
-    role_arn    = null
-    is_enabled  = true
+    name                = "trusted-advisor-cloudwatch-event-rule"
+    schedule_expression = "cron(*/5 * * * ? *)"
+    description         = "Trusted Advisor event rule."
+    role_arn            = null
+    is_enabled          = true
   }
 }
 variable "aws_cloudwatch_event_target" {
