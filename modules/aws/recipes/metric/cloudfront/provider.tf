27b6ac18f89a4937532b11fce55e5bf7ee7953e8
Yoshiaki MIYAZAKI(JP) <yoshiaki.miyazaki@monstar-lab.com>
2021-06-22 10:27:15 +0000

    fixed ci/cd example. fixed warning.

---
+++ modules/aws/recipes/metric/cloudfront/provider.tf
@@ -0,0 +1,12 @@
+#--------------------------------------------------------------
+# Terraform Provider
+#--------------------------------------------------------------
+terraform {
+  required_version = ">=0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.29.1"
+    }
+  }
+}
