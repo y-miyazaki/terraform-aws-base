8f36d99b52509e8d99727b84451b99c5e7f7d2a4
Yoshiaki MIYAZAKI(JP) <30402447+yoshiakimiyazaki@users.noreply.github.com>
2021-06-22 07:02:36 +0000

    fixed docker image. fixed for tfenv.

--- scripts/terraform/aws/check.sh
+++ scripts/terraform/aws/check.sh
@@ -7,6 +7,7 @@ for file in `find /workspace/ -name "main.tf" -type f`; do
     dir=`dirname $file`
     cd ${dir}
     pwd
+    tfenv install
     terraform init -backend=false
     terraform validate
     tflint
--- scripts/terraform/aws/deploy.sh
+++ scripts/terraform/aws/deploy.sh
@@ -4,8 +4,15 @@ DIR=${1:-./}
 
 cd $DIR
 echo "#--------------------------------------------------------------"
-echo "# terraform plan ($PWD)"
+echo "# tfenv install ($PWD)"
+echo "#--------------------------------------------------------------"
+tfenv install
+echo "#--------------------------------------------------------------"
+echo "# terraform init ($PWD)"
 echo "#--------------------------------------------------------------"
 #terraform init -reconfigure -backend-config=terraform."${ENV}".tfbackend
 terraform init
+echo "#--------------------------------------------------------------"
+echo "# terraform plan ($PWD)"
+echo "#--------------------------------------------------------------"
 terraform apply --auto-approve -var-file=terraform."${ENV}".tfvars
