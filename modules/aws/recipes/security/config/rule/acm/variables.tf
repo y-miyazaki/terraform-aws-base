90f8d6f5cafde4ba4e1f76f2172dcc540a48633a
Yoshiaki MIYAZAKI(JP) <ymiyazakixyz@gmail.com>
2022-02-24 10:01:10 +0000

    - add AWS Config for ACM. - update aws provider version for remediation.

---
+++ modules/aws/recipes/security/config/rule/acm/variables.tf
@@ -0,0 +1,30 @@
+#--------------------------------------------------------------
+# module variables
+#--------------------------------------------------------------
+variable "is_enabled" {
+  type        = bool
+  description = "(Optional) A boolean flag to enable/disable AWS Config. Defaults true."
+  default     = true
+}
+variable "name_prefix" {
+  type        = string
+  description = "(Optional) Prefix of config name."
+  default     = ""
+}
+variable "api-gw-endpoint-type-check" {
+  type = object(
+    {
+      input_parameters = map(any)
+    }
+  )
+  default = {
+    input_parameters = {
+      endpointConfigurationTypes = ["REGIONAL", "PRIVATE"]
+    }
+  }
+}
+variable "tags" {
+  type        = map(any)
+  description = "(Optional) Key-value map of resource tags."
+  default     = null
+}
