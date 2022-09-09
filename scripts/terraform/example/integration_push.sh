1ca62c399e97d0340ce7de0007dd27f1bac677a6
Yoshiaki MIYAZAKI(JP) <ymiyazakixyz@gmail.com>
2022-09-09 04:31:33 +0000

    added for project .gitignore file. added for AWS Health Global Service Event. added tfsec:ignore rule. fixed terraform version update. fixed scripts example.

--- scripts/terraform/example/integration_push.sh
+++ scripts/terraform/example/integration_push.sh
@@ -2,6 +2,23 @@
 set -e
 DIR=${1:-./}
 
+if [ -n "${AWS_ACCESS_KEY_ID}" ] && [ -n "${AWS_SECRET_ACCESS_KEY}" ] && [ -n "${AWS_DEFAULT_REGION}" ]; then
+    aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}" --profile default
+    aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}" --profile default
+    aws configure set region "${AWS_DEFAULT_REGION}" --profile default
+else
+    echo "can't set AWS_ACCESS_KEY_ID/AWS_SECRET_ACCESS_KEY/AWS_DEFAULT_REGION."
+    exit 1
+fi
+
+# Create terraform plugin dirctory.
+if [ -n "${TF_PLUGIN_CACHE_DIR}" ]; then
+    mkdir -p "${TF_PLUGIN_CACHE_DIR}"
+else
+    echo "can't set TF_PLUGIN_CACHE_DIR."
+    exit 1
+fi
+
 cd $DIR
 echo "#--------------------------------------------------------------"
 echo "# tfenv install ($PWD)"
@@ -19,7 +36,7 @@ echo "#--------------------------------------------------------------"
 echo "# tfsec ($PWD)"
 echo "#--------------------------------------------------------------"
 tfsec --tfvars-file terraform."${ENV}".tfvars
-echo "#--------------------------------------------------------------"
-echo "# terraform plan ($PWD)"
-echo "#--------------------------------------------------------------"
+# echo "#--------------------------------------------------------------"
+# echo "# terraform plan ($PWD)"
+# echo "#--------------------------------------------------------------"
 terraform plan -lock=false -no-color -var-file=terraform."${ENV}".tfvars
